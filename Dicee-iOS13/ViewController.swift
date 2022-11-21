02e5476176a738ab25d00acde2e3c8318a21fd3b
k-saito-en <saito.kotaro.engineer@gmail.com>
2022-11-21 17:22:21 +0900

    Udemyの学習 基本データ型、乱数生成、配列の扱い方、変数、定数の定義の仕方を学習

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // サイコロ画像のアプリ起動時初期表示を左右それぞれ変更
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+//    var leftDiceNumber = 1
+//    var rightDiceNumber = 5
+    
+    //動作のプロパティ設定なのでIBAction　IB はinterface builderの略
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+
+        //letで変更不可（定数）にした
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+
+        //diceArray[Int.random(in: 0...5)]を簡略化
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        
+//        //左のサイコロの値がボタンを押す度に１ずつ順番に増加していく
+//        leftDiceNumber += 1
+//        //右のサイコロの値がボタンを押す度に１ずつ減少していく
+//        rightDiceNumber -= 1
+        
     }
-
-
+    
+    
 }
 
+    
